bc8707ebfb0d671a5bfe8a6bc7003b50236e0f91
Ben Maharjan <benmaharjan@hotmail.co.uk>
2024-07-23 11:56:23 +0100

    index.js

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 import express from "express";
 const app = express();
 const port = 3000;
+
+app.get('/', (req, res) => {
+    res.status(200).send('Hello LBB7!');  // Sending a response with status code 200
+  });
+  
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);  // Logging a message when the app is listening on the specified port
+  });
