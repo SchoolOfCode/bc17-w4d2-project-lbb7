c07e4d72ac86c4d1de3f090cc25201aa7ae4893e
Ben Maharjan <benmaharjan@hotmail.co.uk>
2024-07-25 11:04:26 +0100

    PoST working

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import express from "express";
 import helmet from 'helmet';
+import { v4 as uuidv4 } from 'uuid';
+
 const app = express();
 const port = 4000;
 
@@ -9,53 +11,50 @@ const activities = [
     "activity_submitted": "1719486190058",
     "activity_type": "run",
     "activity_duration": "30"
-    }
+  }
 ];
-
 
 app.use(helmet());
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.status(200).send('Hello LBB7!');  // Sending a response with status code 200
-  });
+  res.status(200).send('Hello LBB7!');  // Sending a response with status code 200
+});
 
 app.get('/activities', (req, res) => {
-    res.status(200).json({
-      "error": null,
-      "data" : activities
-    });  // Sending a response with status code 200
-  });
-  
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);  // Logging a message when the app is listening on the specified port
+  res.status(200).json({
+    "error": null,
+    "data": activities
+  });  // Sending a response with status code 200
 });
 
 app.post('/activities', (req, res) => {
-    const newActivity = req.body.newActivity;  // Assuming the request body contains an 'activity' object
-    activies.push(newActivity);
-  console.log(newActivity);
+  const newActivity = req.body;  // Assuming the request body contains the new activity object
 
-    if (!newActivity) {
-      // Sending a 400 Bad Request response if no activity data is sent
-       res.status(400).json({ 
-        "error": true,
+  if (!newActivity || !newActivity.activity_type || !newActivity.activity_duration) {
+    // Sending a 400 Bad Request response if no activity data is sent or if required fields are missing
+    return res.status(400).json({ 
+      "error": true,
       "data": null 
     });
-    }
+  }
 
-    const activity = {
-        ...newActivity,
-        id: uuid4(),
-        activity_submitted: date.now(), 
-    }
-    activities.push(activity);
-    console.log(activity);
-  
-    // You can process the activity data here (e.g., save it to a database)
-  
-    // Sending a response with status code 200
-    res.status(200).send({ message: 'Activity received', activity });
+  const activity = {
+    ...newActivity,
+    id: 12345, //uuid4(),
+    activity_submitted: Date.now()
+  };
+
+  activities.push(activity);
+  console.log(activity);
+
+  // Sending a response with status code 200
+  res.status(200).send({ 
+    message: 'Activity received', 
+    activity 
+  });
 });
 
-//TO RUN, OPEN TERMINAL AND TYPE "node index.js" THEN TYPE "http://localhost:3002/" INTO GOOGLE OR POSTMAN
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);  // Logging a message when the app is listening on the specified port
+});
